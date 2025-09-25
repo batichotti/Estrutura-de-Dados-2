bfc249899a58f02c74d35b7637b513e6b6850352
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2025-09-25 17:05:05 -0300

    Update quick_sort.cpp

--- aulas/2 - ordenacao/quick_sort.cpp
+++ aulas/2 - ordenacao/quick_sort.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 void troque(int* v, int a, int b){
     int temp = v[a];
     v[a] = v[b];
@@ -22,4 +24,19 @@ void Quicksort(int* v, int p, int r){
         Quicksort(v, p, q-1);
         Quicksort(v, q+1, r);
     }
+}
+
+
+int main() {
+    int v[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(v) / sizeof(v[0]);
+
+    Quicksort(v, 0, n - 1);
+
+    std::cout << "Vetor ordenado: ";
+    for (int i = 0; i < n; i++)
+        std::cout << v[i] << " ";
+    std::cout << std::endl;
+
+    return 0;
 }
